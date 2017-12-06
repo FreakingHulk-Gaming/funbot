b63439a3ce4ded68977d7bca7a678f6f32d1c53e
FHGDev <gideon.lingle14@gmail.com>
2017-12-05 20:17:30 -0600

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,18 @@
+// discord constants
+d = require('discord.js');
+bot = new d.Client();
+prefix = fun
+bot.login(process.env.tok)
+// end
+
+//enmap shit
+const Enmap = require("enmap");
+const EnmapLevel = require("enmap-level");
+
+const pointProvider = new EnmapLevel({name: "points"});
+this.points = new Enmap({provider: pointProvider});
+//end
+
+//events
+
+//end
